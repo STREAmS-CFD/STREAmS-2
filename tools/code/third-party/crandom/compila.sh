6bcd942bc74510e10fa7b8a2cd55904212f90e93
Francesco Salvadore <fsalvado@login05.leonardo.local>
2025-04-09 17:12:57 +0200

    removed duplicated source

--- tools/code/third-party/crandom/compila.sh
+++
@@ -1,4 +0,0 @@
-gcc crandom.c -c
-gfortran crandom_f.F90 -c
-gfortran test_f.F90 -c
-gfortran test_f.o crandom_f.o crandom.o 
--- tools/code/third-party/crandom/test.c
+++
@@ -1,8 +0,0 @@
-int main(void) {
-    double a;
-    init_crandom(10);
-    get_crandom(&a);
-    printf("extracted random %lf ",a);
-    return 0;
-};
-
